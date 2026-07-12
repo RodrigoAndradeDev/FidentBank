3b0882176fbd7f4256d641f946de92dffa7f70a1
Rodrigo Andrade <rodrigoandrade.santos@ucsal.edu.br>
2026-07-12 17:30:00 -0300

    chore: configuracao inicial do projeto

---
+++ Fident-Backend/fidentBank/src/main/java/dev/Rodrigo/fidentBank/FidentBankApplication.java
@@ -0,0 +1,13 @@
+package dev.Rodrigo.fidentBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidentBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidentBankApplication.class, args);
+	}
+
+}
