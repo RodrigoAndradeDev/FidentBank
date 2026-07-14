0f8ff7ceac36cc361db9cae756a90a2e196243fa
Rodrigo Andrade <rodrigo.anddev@gmail.com>
2026-07-14 07:44:41 -0300

    feat: implementado a Classe Usuario (model) completamente!

--- Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/model/Conta.java
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/model/Conta.java
@@ -1,12 +1,41 @@
 package dev.rodrigo.fidentbank.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@Table(name = "tb_conta")
 public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @Column(name = "numero_conta", nullable = false, unique = true)
+    private String numeroConta;
+
+    @Column(name = "saldo", nullable = false)
+    private BigDecimal saldo;
+
+    @Column(name = "limite", nullable = false)
+    private BigDecimal limite;
     
 }
