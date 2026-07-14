6af8cb3c405b52ee4c3acffdf5837e4f69b76cb6
Rodrigo Andrade <rodrigo.anddev@gmail.com>
2026-07-14 09:37:30 -0300

    feat: Criado a interface ContaRepository

---
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/repositories/ContaRepository.java
@@ -0,0 +1,12 @@
+package dev.rodrigo.fidentbank.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.rodrigo.fidentbank.model.Conta;
+
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+    
+}
