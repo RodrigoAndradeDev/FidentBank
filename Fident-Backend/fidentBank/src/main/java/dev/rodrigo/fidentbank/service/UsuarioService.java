a4f253de09190a7d8440020ada7666e1aa0921f6
Robert <robert.alegria17@gamil.com>
2026-07-18 10:11:12 -0300

    feat: Adicionado a dependencia da criptografia com o junit

--- Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/service/UsuarioService.java
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package dev.rodrigo.fidentbank.service;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-    
+    //BasicTextEncryptor textEncryptor = new BasicTextEncryptor(); 
+   // textEncryptor.setPasswordCharArray
 }
