a7a22668507a349f73c6c6d200e6f8413c53e635
Rodrigo Andrade <rodrigo.anddev@gmail.com>
2026-07-20 18:07:45 -0300

    fix: correct generic type declaration in findByNumeroConta method in ContaRepository

--- Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/repositories/ContaRepository.java
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/repositories/ContaRepository.java
@@ -10,6 +10,6 @@ import dev.rodrigo.fidentbank.model.Conta;
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
     boolean numeroExiste(String numeroGerado);
-    <Optional> Conta findByNumeroConta(String numeroConta);
+    java.util.Optional<Conta> findByNumeroConta(String numeroConta);
     
 }
