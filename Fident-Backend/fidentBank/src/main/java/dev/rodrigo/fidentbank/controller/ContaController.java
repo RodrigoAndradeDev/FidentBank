1bf36ab9f852ddd946054c534f3ba0bfb9621ffb
Rodrigo Andrade <rodrigo.anddev@gmail.com>
2026-07-31 18:37:19 -0300

    feat: adicionado o metodo de get de todos os numeros de contas!

--- Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/controller/ContaController.java
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/controller/ContaController.java
@@ -1,11 +1,15 @@
 package dev.rodrigo.fidentbank.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.rodrigo.fidentbank.dto.ContaNumeroResponseDto;
 import dev.rodrigo.fidentbank.dto.ContaResponseDto;
 import dev.rodrigo.fidentbank.model.Conta;
 import dev.rodrigo.fidentbank.service.ContaService;
@@ -29,11 +33,11 @@ public class ContaController {
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping()
-    public ResponseEntity<conta> buscarTodasContas() {
-        Conta conta = contaService.buscarTodasContas();
-        ContaResponseDto responseDto =  new ContaResponseDto(
-                conta.getUsuario().getNome(),
+    @GetMapping("/numeros")
+    public ResponseEntity<Page<ContaNumeroResponseDto>> buscarTodasContas(
+       @PageableDefault(size = 10, sort = {"numero"}) Pageable paginacao){
+        Page<ContaNumeroResponseDto> pagina = contaService.buscarContaPorNumero(paginacao);
+        return ResponseEntity.ok(pagina);
     }
 
 }
--- Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/repositories/ContaRepository.java
+++ Fident-Backend/fidentBank/src/main/java/dev/rodrigo/fidentbank/repositories/ContaRepository.java
@@ -1,6 +1,10 @@
 package dev.rodrigo.fidentbank.repositories;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.rodrigo.fidentbank.model.Conta;
@@ -13,4 +17,7 @@ public interface ContaRepository extends JpaRepository<Conta, Long> {
     boolean existsByNumeroConta(String numeroConta);
     java.util.Optional<Conta> findByNumeroConta(String numeroConta);
     
+    @Query("Select c.numeroConta FROM Conta c")
+    Page<String> findAllNumeroConta(Pageable pageable);
+
 }
